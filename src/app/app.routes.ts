9025ad00b03ba0d81a60e8b229836f1a3fa0f303
trinhdinhthanhyahoo <167999502+trinhdinhthanhyahoo@users.noreply.github.com>
2024-12-04 17:22:50 +0700

    ADD product-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { OrderComponent } from './order/order.component';
 import { OrderComfirmComponent } from './order-comfirm/order-comfirm.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 export const routes: Routes = [
     // { path: '', component: HomeComponent },
     // { path: 'home', component: HomeComponent }
     // { path: '', component: OrderComponent }
     // { path: '', component: OrderComfirmComponent },
-    { path: '', component: LoginComponent }
+    // { path: '', component: LoginComponent },
+    // { path: '', component: RegisterComponent },
+    { path: '', component: DetailProductComponent }
 ];
